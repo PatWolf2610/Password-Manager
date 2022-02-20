6283c5d46f6c95234d2ca7fa3aff4e77466ed980
VietNguyenWolf <nguyenhoangviethustk63@gmail.com>
2022-02-20 19:43:54 +0700

    searchbox added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from operator import ge
 from tkinter import *
 from tkinter import messagebox # not a class
 from random import choice, randint,shuffle
+import json
 
 FONT = ('Arial',10,'bold')
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -27,17 +29,52 @@ def save_insert():
     web_insert = web_entry.get()
     eu_insert = eu_entry.get()
     pass_insert = pass_entry.get()
+    new_data = {
+        web_insert: {
+            "email" : eu_insert,
+            "password" : pass_insert
+        }
+    }
     if web_insert == '' or eu_insert == '' or pass_entry == '':
         messagebox.showerror(title='Invalid Input',message='Do not let any field empty')
     else:
         is_ok =messagebox.askokcancel(title=web_insert,message=f"Do you want to save: \nEmail: {eu_insert}\nPassword: {pass_insert}")
         if is_ok == True:
-            with open('data.txt','a') as f:
-                f.write(f"Website: {web_insert} | Email: {eu_insert} | Password: {pass_insert}"+'\n')
-            web_entry.delete(0,END)
-            pass_entry.delete(0,END) 
+            try:
+                with open('data.json','r') as f:
+                    data = json.load(f)
+            except FileNotFoundError:
+                with open('data.json','w') as f:
+                    json.dump(new_data,f,indent=4)
+            except json.decoder.JSONDecodeError:
+                with open('data.json','w') as f:
+                    json.dump(new_data,f,indent=4)
+            else:
+                data.update(new_data)
+                with open('data.json','w') as f:
+                    json.dump(data,f,indent=4)
+            finally:
+                web_entry.delete(0,END)
+                pass_entry.delete(0,END) 
         else:
             pass
+# ---------------------------- Search for information ------------------------------- #
+def find_password():
+    try:
+        with open('data.json','r') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        messagebox.showinfo(title='Searching password',message='There is no save info in database. Please save your forst information')
+        with open('data.json','w') as f:
+            pass
+    except json.decoder.JSONDecodeError:
+        messagebox.showinfo(title='Searching password',message='There is no save info in database. Please save your forst information')
+    else:
+        website_name = web_entry.get()
+        if website_name in data.keys():
+            messagebox.showinfo(title=f"Search password",message=f"Website: {website_name}\nEmail/Username: {data[website_name]['email']}\nPassword: {data[website_name]['password']}")
+        else:
+            messagebox.showinfo(title='Searching password',message=f'There is no saved info for {website_name}')
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Password Manager')
@@ -63,11 +100,11 @@ pass_label = Label(text='Password',font=FONT)
 pass_label.grid(row=3,column=0)
 
 #websit entry
-web_entry = Entry(width=55)
-web_entry.grid(row=1,column=1,columnspan=2)
+web_entry = Entry(width=34)
+web_entry.grid(row=1,column=1)
 web_entry.focus()
 #e/u entry
-eu_entry = Entry(width=55)
+eu_entry = Entry(width=56)
 eu_entry.grid(row=2,column=1,columnspan=2)
 eu_entry.insert(0,'@gmail.com')
 # password entry
@@ -80,5 +117,7 @@ gen_button.grid(row=3,column=2)
 # add button
 add_button = Button(text='Add',width=47,command=save_insert)
 add_button.grid(row=4,column=1,columnspan=2)
-
+# search button
+search_button = Button(text='Search',width=17,command=find_password)
+search_button.grid(row=1,column=2)
 window.mainloop()
